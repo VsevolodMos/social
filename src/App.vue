a2c65d374a4a5317a15385f30d46f9da541911bd
VsevolodMos <vsevolodmos2005@gmail.com>
2020-02-03 23:55:34 +0300

    Almost completed navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,73 @@
 <template>
-  <v-app id ="app">
-      <v-navigation-drawer>
-          <v-content>
-            <v-container fluid>
-               <router-view/>
-            </v-container>
-          </v-content>
-      </v-navigation-drawer>
+  <v-app id="app">
+    <v-navigation-drawer
+       color="green"
+        dark
+        expand-on-hover
+        hide overlay
+        permament
+        right
+        app
+       >
+        <v-list
+      nav 
+      shaped
+      dense
+     >
+     <v-list-item two-line>
+      <v-list-item-avatar>
+         <img :src="'https://randomuser.me/api/portraits/men/1.jpg'"> 
+      </v-list-item-avatar>
+
+      <v-list-item-content class="text-left">
+         <v-list-item-title class="font-weight-black">SocialLink</v-list-item-title>
+         <v-list-item-subtitle>Иван Иванов</v-list-item-subtitle>
+      </v-list-item-content>
+     </v-list-item>
+    <v-divider class="my-3"></v-divider>
+    
+    <v-list-item link :to="путь">
+
+      <v-list-item-icon>
+        <v-icon>mdi-home-outline</v-icon>
+      </v-list-item-icon>
+
+      <v-list-item-content>
+        <v-list-item-title class="text-left">Главная</v-list-item-title>
+      </v-list-item-content>
+
+    </v-list-item>
+
+    <v-list-item link :to="путь">
+
+      <v-list-item-icon>
+        <v-icon>mdiAccount</v-icon>
+      </v-list-item-icon>
+
+      <v-list-item-content>
+        <v-list-item-title class="text-left">Мой профиль</v-list-item-title>
+      </v-list-item-content>
+
+    </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+    
+    <v-content class="px-12 py-3">
+      <v-container fluid>
+        <router-view/>
+      </v-container>
+    </v-content>
+
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
   },
 
   data: () => ({
